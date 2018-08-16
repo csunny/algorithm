5ab96223f10ace27de1d17a722919cd462be6488
csunny <cfqcsunny@gmail.com>
2018-08-16 22:08:33 +0800

    实现冒泡排序

--- algorithm/bubble_sort.py
+++ algorithm/bubble_sort.py
@@ -11,4 +11,20 @@ def bubble_sort(values):
     :param values:
     :return:
     """
-    pass
+    length = len(values) - 1
+
+    if length <= 1:
+        return values
+
+    for i, _ in enumerate(values):
+        for j in range(length, i, -1):
+            if values[j] < values[j - 1]:
+                values[j], values[j - 1] = values[j - 1], values[j]
+
+    return values
+
+
+if __name__ == '__main__':
+    v = [3, 4, 5, 2, 7, 9, 8, 1]
+    sort_v = bubble_sort(v)
+    print(sort_v)
