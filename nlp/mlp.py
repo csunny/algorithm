cded7ba277e94a3283d7a5a11889a58461e6c9bd
csunny <cfqsunny@163.com>
2023-03-27 22:04:35 +0800

    mlp inpl

--- nlp/mlp.py
+++ nlp/mlp.py
@@ -21,3 +21,21 @@ W2 = nn.Parameter(torch.randn(num_hiddens, num_outputs, requires_grad=True) * 0.
 b2 = nn.Parameter(torch.zeros(num_outputs, requires_grad=True))
 
 params = [W1, b1, W2, b2]
+
+
+def relu(X):
+    a = torch.zeros_like(X)
+    return torch.max(X, a)
+
+
+def net(X):
+    X = X.reshape((-1, num_inputs))
+    H = relu(X@W1 + b1)    # @代表矩阵乘法
+    return (H@W2 + b2)
+
+loss = nn.CrossEntropyLoss(reduction='none')
+num_epochs, lr = 10, 0.1
+updater = torch.optim.SGD(params, lr=lr)
+
+d2l.train_ch3(net, train_iter, test_iter, loss, num_epochs, updater)
+d2l.predict_ch3(net, test_iter)
